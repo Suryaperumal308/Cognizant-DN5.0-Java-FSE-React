5febebe59a6844856ac3a9a6cba9043c17405647
P Surya <psurya03082006@gmail.com>
2026-07-15 13:12:58 +0530

    Added Week 5 ReactJS HOL 4 Blog App Lifecycle Methods

---
+++ WEEK-5/blogapp/src/Posts.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import Post from "./Post";
+
+class Posts extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts: [],
+        };
+    }
+
+    loadPosts() {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then((response) => response.json())
+            .then((data) => {
+                const posts = data.map(
+                    (item) => new Post(item.id, item.title, item.body)
+                );
+
+                this.setState({ posts });
+            });
+    }
+
+    componentDidMount() {
+        this.loadPosts();
+    }
+
+    componentDidCatch(error) {
+        alert("Error: " + error);
+    }
+
+    render() {
+        return (
+            <div style={{ padding: "20px" }}>
+                <h1>Blog Posts</h1>
+
+                {this.state.posts.map((post) => (
+                    <div
+                        key={post.id}
+                        style={{
+                            border: "1px solid gray",
+                            margin: "10px",
+                            padding: "10px",
+                        }}
+                    >
+                        <h3>{post.title}</h3>
+                        <p>{post.body}</p>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+export default Posts;
